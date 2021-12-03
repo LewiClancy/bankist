0553cf94dfc7a65679f9a8ae4371ea89826e97f4
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-03 07:49:46 +0300

    Removed the uneeded dashboard component.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MiniTransactionsComponent } from './mini-transactions/mini-transactions.component';
 import { MiniSummaryComponent } from './mini-summary/mini-summary.component';
 import { MiniUserInfoComponent } from './mini-user-info/mini-user-info.component';
-import { DashboardLinkComponent } from './dashboard-link/dashboard-link.component';
 import { SharedModule } from '../shared/shared.module';
 import { LinkHoverDirective } from './link-hover-directive/link-hover.directive';
 
@@ -16,7 +14,6 @@ import { LinkHoverDirective } from './link-hover-directive/link-hover.directive'
     MiniTransactionsComponent,
     MiniSummaryComponent,
     MiniUserInfoComponent,
-    DashboardLinkComponent,
     LinkHoverDirective,
   ],
   imports: [SharedModule, DashboardRoutingModule],
