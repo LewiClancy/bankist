6405053a1eef7bd6754b2046bc6c165cb6ed1bf2
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-08 20:27:13 +0300

    Rearranged the core module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AngularMaterialModule } from '../material.module';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { LinkItemComponent } from './link-item/link-item.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LinkItemComponent } from './components/link-item/link-item.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
