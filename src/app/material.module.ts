8e25e67f23e90644fbacc4b562562ce84434320c
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-08 20:58:29 +0300

    fixed sidenav and nav margins

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,12 +12,14 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 const MaterialComponents = [
   MatToolbarModule,
   MatButtonModule,
   MatRippleModule,
   MatIconModule,
+  MatSidenavModule,
   MatFormFieldModule,
   MatInputModule,
   MatDividerModule,
