03d342df3cb97e154b802f0d02b87598b005e9f4
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-11 14:50:59 +0300

    style: Fix the margin top

--- src/app/dashboard/mini-transactions/mini-transactions.component.ts
+++ src/app/dashboard/mini-transactions/mini-transactions.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Transaction } from 'src/app/core/models';
 import { TransactionsService } from 'src/app/core/services';
 
@@ -8,16 +10,29 @@ import { TransactionsService } from 'src/app/core/services';
   templateUrl: './mini-transactions.component.html',
   styleUrls: ['./mini-transactions.component.scss'],
 })
-export class MiniTransactionsComponent implements OnInit {
+export class MiniTransactionsComponent implements OnInit, OnDestroy {
   dataSource!: MatTableDataSource<Transaction>;
-
+  transactionSub!: Subscription;
   columnsToDisplay = ['account', 'type', 'amount'];
 
-  constructor(private transactionsService: TransactionsService) {}
+  constructor(
+    private transactionsService: TransactionsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.transactionsService.transactions.subscribe((transactions) => {
-      this.dataSource = new MatTableDataSource(transactions);
-    });
+    this.transactionSub = this.transactionsService.transactions.subscribe(
+      transactions => {
+        this.dataSource = new MatTableDataSource(transactions);
+      }
+    );
+  }
+
+  viewTrandactionStatement() {
+    this.router.navigate(['transaction-statements']);
+  }
+
+  ngOnDestroy(): void {
+    this.transactionSub.unsubscribe();
   }
 }
