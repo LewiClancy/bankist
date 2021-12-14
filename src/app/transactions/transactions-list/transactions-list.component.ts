e40ce33bac3feaf720355af9b63aadb8f9a77efb
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-14 20:52:53 +0300

    feat: Add a material table.

--- src/app/transactions/transactions-list/transactions-list.component.ts
+++ src/app/transactions/transactions-list/transactions-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
+import { Transaction } from 'src/app/core/models';
+import { TransactionsService } from 'src/app/core/services';
 
 @Component({
   selector: 'app-transactions-list',
   templateUrl: './transactions-list.component.html',
-  styleUrls: ['./transactions-list.component.scss']
+  styleUrls: ['./transactions-list.component.scss'],
 })
-export class TransactionsListComponent implements OnInit {
+export class TransactionsListComponent implements OnInit, AfterViewInit {
+  dataSource!: MatTableDataSource<Transaction>;
+  transactionSub!: Subscription;
+  columnsToDisplay = ['date', 'accountId', 'kind', 'amount'];
 
-  constructor() { }
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngOnInit(): void {
+  constructor(private transactionsService: TransactionsService) {}
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
+  ngOnInit(): void {
+    this.transactionSub = this.transactionsService.transactions.subscribe(
+      transactions => {
+        this.dataSource = new MatTableDataSource(transactions);
+      }
+    );
+  }
 }
