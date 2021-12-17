02852f833f1b0335f5fa0ecbf02df69906c3d8af
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-18 00:11:58 +0300

    Create owner and account models

---
+++ src/app/core/models/account-owner.model.ts
@@ -0,0 +1,7 @@
+export interface AccountOwner {
+  id: string;
+  firstName: string;
+  lastName: string;
+  photoUrl: string;
+  email: string;
+}
--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -1,1 +1,3 @@
 export * from './transaction.model';
+export * from './account-owner.model';
+export * from './account.model';
