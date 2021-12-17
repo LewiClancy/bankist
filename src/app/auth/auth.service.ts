7175140869ab10d747a8d600f414c7c3f85d9746
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-18 00:10:13 +0300

    feat: Adda login function

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private afs: AngularFireAuth) {}
+
+  handleLogin(email: string, password: string) {
+    return this.afs.signInWithEmailAndPassword(email, password);
+  }
+}
