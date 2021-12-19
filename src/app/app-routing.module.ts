04118d5db138a30d3c6e277ab00ffbd888061e40
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-19 18:58:49 +0300

    Add canLoad guard to dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
@@ -11,7 +12,9 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canLoad: [AuthGuard],
   },
+
   {
     path: 'transactions',
     loadChildren: () =>
