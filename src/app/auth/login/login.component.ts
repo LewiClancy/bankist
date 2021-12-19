80ce9dd276a15462ee80ca10e04e2e9637d86c48
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-19 17:33:07 +0300

    style: Add some padding

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
