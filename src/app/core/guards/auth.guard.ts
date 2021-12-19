8ae71cfd0a9b0e9e62505eddb01f87089f391d54
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-19 18:48:01 +0300

    feat: Add auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectIsLoggedIn } from 'src/app/auth/store/auth.selectors';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanLoad {
+  returnType!: boolean | UrlTree;
+
+  constructor(store: Store, router: Router) {
+    store.select(selectIsLoggedIn).subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.returnType = true;
+      } else {
+        console.log('here');
+        this.returnType = router.createUrlTree(['/auth', 'login']);
+      }
+    });
+  }
+
+  canLoad() {
+    return this.returnType;
+  }
+}
