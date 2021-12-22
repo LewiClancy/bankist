39705fd6da8965d29768b941f00cfb6ba51fa9ce
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-22 15:09:04 +0300

    Initialize the dashboard module store and effects

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,6 +6,10 @@ import { MiniTransactionsComponent } from './mini-transactions/mini-transactions
 import { MiniSummaryComponent } from './mini-summary/mini-summary.component';
 import { MiniUserInfoComponent } from './mini-user-info/mini-user-info.component';
 import { SharedModule } from '../shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromDashboard from './store';
+import { EffectsModule } from '@ngrx/effects';
+import { DashboardEffects } from './store/dashboard.effects';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,14 @@ import { SharedModule } from '../shared/shared.module';
     MiniSummaryComponent,
     MiniUserInfoComponent,
   ],
-  imports: [SharedModule, DashboardRoutingModule],
+  imports: [
+    SharedModule,
+    DashboardRoutingModule,
+    EffectsModule.forFeature([DashboardEffects]),
+    StoreModule.forFeature(
+      fromDashboard.dashboardFeatureKey,
+      fromDashboard.DashboardReducer
+    ),
+  ],
 })
 export class DashboardModule {}
