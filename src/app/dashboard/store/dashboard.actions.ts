9e9e3ae6b420202490b0f15be9e4d5a7589fbe6a
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-22 15:04:35 +0300

    Create the dashboard actions

---
+++ src/app/dashboard/store/dashboard.actions.ts
@@ -0,0 +1,30 @@
+import { createAction, props } from '@ngrx/store';
+import { Account, AccountOwner } from 'src/app/core/models';
+
+export const loadAccountOwner = createAction(
+  '[Auth Effects] Load Account Owner Information',
+  props<{ accountOwnerId: string }>()
+);
+
+export const loadAccountOwnerSuccessful = createAction(
+  '[Dashboard Effect] Load Account Owner Information Succeeded',
+  props<{ accountOwner: AccountOwner }>()
+);
+
+export const loadAccountOwnerFailed = createAction(
+  '[Dashboard Effect] Load Account Owner Information Failed'
+);
+
+export const loadAccountInfo = createAction(
+  '[Dashboard Effect] Load Account Info',
+  props<{ accountId: string }>()
+);
+
+export const loadAccountInfoSuccess = createAction(
+  '[DashboardEffect] Account Info Loaded Successfully',
+  props<{ accountInfo: Account }>()
+);
+
+export const loadAccountInfoNotSuccess = createAction(
+  '[DashboardEffect] Account Info Loaded Successfully'
+);
