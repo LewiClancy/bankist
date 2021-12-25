7029f1caf2277d07be884d0f8edac7aa039a6a1a
Lewis Ndungu <itslewisndungu@gmail.com>
2021-12-25 12:56:23 +0300

    refactor: dashboard component The component now gets info direct from the store.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,27 +15,15 @@ import * as dashboardSelectors from './store/dashboard.selectors';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  // accountOwnerInfo$: Observable<Partial<AccountOwner>> = of({
-  //   firstName: 'Lewis',
-  //   surname: "Ndung'u",
-  //   photoUrl: '../../assets/images/image-jeremy.png',
-  // });
-
   accountOwnerInfo$!: Observable<AccountOwner | undefined>;
-  // accountInfo$: Observable<Partial<Account>> = of({
-  //   id: 'fjdklaJKlfjaljlfaj',
-  //   float: 102320,
-  //   status: 'active',
-  // });
 
   accountInfo$!: Observable<Account | undefined>;
 
-  // transactions$ = this.transactionsService.transactions;
   transactions$!: Observable<Transaction[]>;
 
   isLoading$!: Observable<boolean>;
 
-  constructor(private store: Store<AppState>, private titleService: Title) {
+  constructor(private store: Store<AppState>, titleService: Title) {
     titleService.setTitle('Dashboard | Bankist');
   }
 
