e46ed423d39b0643ee676a180c15250096fb5bd8
Lewis Ndungu <itslewisndungu@gmail.com>
2022-01-25 20:29:20 +0300

    refactor(dashboard): Change title in OnInit and not constructor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,10 +5,8 @@ import { Observable } from 'rxjs';
 import { Account, AccountOwner, Transaction } from '../core/models';
 import { AppState } from '../store';
 import { selectIsLoading } from '../store/selectors/loading.selectors';
-
 import * as accountSelectors from '../store/selectors/account.selectors';
 import { selectUser } from '../store/selectors/auth.selectors';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,15 +19,11 @@ export class DashboardComponent implements OnInit {
   isAppLoading$!: Observable<boolean>;
   transactions$!: Observable<Transaction[]>;
 
-  constructor(
-    private store: Store<AppState>,
-    private router: Router,
-    titleService: Title
-  ) {
-    titleService.setTitle('Dashboard | Bankist');
-  }
+  constructor(private store: Store<AppState>, private titleService: Title) {}
 
   ngOnInit(): void {
+    this.titleService.setTitle('Dashboard | Bankist');
+
     this.accountOwnerInfo$ = this.store.select(selectUser);
 
     this.accountInfo$ = this.store.select(accountSelectors.selectAccountInfo);
